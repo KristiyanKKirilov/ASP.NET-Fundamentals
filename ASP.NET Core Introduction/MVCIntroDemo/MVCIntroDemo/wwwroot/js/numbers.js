5ea866f4c402a22a08b6541d18cc5e5c14e94806
Kristiyan Kirilov <124174959+KristiyanKKirilov@users.noreply.github.com>
2024-02-20 01:09:34 +0200

    Delete ASP.NET Core Introduction directory

--- ASP.NET Core Introduction/MVCIntroDemo/MVCIntroDemo/wwwroot/js/numbers.js
+++
@@ -1,5 +0,0 @@
-function SetLimit() {
-    let num = document.getElementById("limitInput").value || 50;
-
-    window.location = "https://localhost:7246/Numbers/Limit?limit=" + num;
-}
--- ASP.NET Core Introduction/TextSplitter/TextSplitter/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TextSplitter.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
