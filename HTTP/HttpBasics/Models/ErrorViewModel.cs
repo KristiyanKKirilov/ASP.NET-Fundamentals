d799c88f6672b2c3aa10c6963df07f8f8dcee74e
Kristiyan Kirilov <124174959+KristiyanKKirilov@users.noreply.github.com>
2024-02-20 01:10:16 +0200

    Delete HTTP directory

--- HTTP/HttpBasics/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HttpBasics.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- HTTP/HttpBasics/Models/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HttpBasics.Models
-{
-    public class Item
-    {
-        public int Id { get; set; }
-        public string Description { get; set; }
-        public int Quantity { get; set; }
-    }
-}
